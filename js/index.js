78ae4cdc4e71a7f2b0682413870e0a66875d02e2
laimengliang <564606073@qq.com>
2017-06-30 21:51:11 +0800

    静态结束

--- js/index.js
+++ js/index.js
@@ -11,23 +11,22 @@ $(function(){
 		$('.zix_x').hide();
 	})
 	//添加banner图片
-	$.get("json/banner.json",function(d){     //获取图片
+	$.get("../json/banner.json",function(d){     //获取图片
 		var arr = d;
 		for(var i = 0 ; i < arr.length ; i ++){
 			var obj = arr[i];
-//			console.log(113)
+//			console.log(i)
 			var str = "<div class='content'><a href='#'>";
 //			$("<div class='content'><a href='#'></a></div>").appendTo($("#index_lb"))
 			for(var j = 0; j < obj.img.length ; j ++){
 				str += `<div style='background: url("${obj.img[j]}") center; height: 90px'></div>`;
 //				$("<div></div>").appendTo($("#index_lb>div>a:last"));
-//				console.log(111)
 //				$("#index_lb a div:last").css({"background":"url(" + obj.img[j] + ") center",height:"90px"})
 			}
 			str += "</a></div>" 
 //			var jImg = obj.img.pop()
 //			console.log(jImg)
-			console.log(str)
+//			console.log(str)
 			$(str).appendTo($('#index_lb'))
 
 		}
@@ -132,6 +131,71 @@ $(function(){
 			
 			
 		})
+//韩风时尚女装添加图片
+		
+		$.get("../json/goods.json",function(d){
+			let arr = d;
+			let obj1 = arr[0];
+			let obj2 = arr[1];
+			let str = "<ul class='list'>";
+			for(let i = 0; i < obj1.img280.length ; i ++){
+				str += `<li><a href="#" style="background:url(${obj1.img280[i]}) no-repeat"></a><span>￥</span><span>${obj1.promotionalPrice}</span><span>￥${obj1.price}</span><div>立即购买</div></li>`
+			}
+			str+="</ul><ul class='list'>";
+			for(let j = 0; j < obj2.img280.length ; j ++){
+				str += `<li><a href="#" style="background:url(${obj2.img280[j]}) no-repeat"></a><span>￥</span><span>${obj2.promotionalPrice}</span><span>￥${obj2.price}</span><div>立即购买</div></li>`
+			}
+			str+="</ul>";
+			$(str).appendTo($(".main_hStyle"));
+		})
+//韩风时尚妈妈添加图片		
+		$.get("../json/goods.json",function(d){
+			let arr = d;
+			let obj1 = arr[2];
+			let str = "<ul class='list'>";
+			for(let i = 0; i < obj1.img280.length ; i ++){
+				str += `<li><a href="#" style="background:url(${obj1.img280[i]}) no-repeat"></a><span>￥</span><span>${obj1.promotionalPrice}</span><span>￥${obj1.price}</span><div>立即购买</div></li>`
+			}
+			str+="</ul>";
+			$(str).appendTo($(".main_hStyleMother"));
+		})
+//韩风时尚少女	添加图片	
+		$.get("../json/goods.json",function(d){
+			let arr = d;
+			let obj1 = arr[3];
+			let str = "<ul class='list'>";
+			for(let i = 0; i < obj1.img280.length ; i ++){
+				str += `<li><a href="#" style="background:url(${obj1.img280[i]}) no-repeat"></a><span>￥</span><span>${obj1.promotionalPrice}</span><span>￥${obj1.price}</span><div>立即购买</div></li>`
+			}
+			str+="</ul>";
+			$(str).appendTo($(".main_hStyleGirl"));
+		})
+//	韩风时尚男童添加图片		
+		$.get("../json/goods.json",function(d){
+			let arr = d;
+			let obj1 = arr[4];
+			let str = "<ul class='list'>";
+			for(let i = 0; i < obj1.img280.length ; i ++){
+				str += `<li><a href="#" style="background:url(${obj1.img280[i]}) no-repeat"></a><span>￥</span><span>${obj1.promotionalPrice}</span><span>￥${obj1.price}</span><div>立即购买</div></li>`
+			}
+			str+="</ul>";
+			$(str).appendTo($(".main_hStyleBoy"));
+		})
+//		欧美时尚添加图片	
+		$.get("../json/goods.json",function(d){
+			let arr = d;
+			let obj1 = arr[0];
+			let str = "<ul class='list'>";
+			for(let i = 0; i < obj1.img280.length ; i ++){
+				str += `<li><a href="#" style="background:url(${obj1.img280[i]}) no-repeat"></a><span>￥</span><span>${obj1.promotionalPrice}</span><span>￥${obj1.price}</span><div>立即购买</div></li>`
+			}
+			str+="</ul>";
+			$(str).appendTo($(".main_eStyle"));
+		})
+		
+		
+		
+		
 //返回顶部
 		$(".float_box").eq(3).click(function(e){
 			e.preventDefault();
